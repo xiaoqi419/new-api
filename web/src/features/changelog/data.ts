3ec39796cf84b122e2171394115dcae6c2a951eb
xiaolewo <1789408080@qq.com>
2026-08-03 16:20:58 +0800

    docs(changelog): 填入本轮镜像 tag

--- web/src/features/changelog/data.ts
+++ web/src/features/changelog/data.ts
@@ -29,7 +29,7 @@ import type { ChangelogEntry } from './types'
 
 export const CHANGELOG: ChangelogEntry[] = [
   {
-    version: 'unreleased',
+    version: '20260803-f78002d6',
     date: '2026-08-03',
     changes: [
       {
